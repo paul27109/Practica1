29b838ee3b0c9903e0c798038078c98860b9d46d
Paul Montenegro <44028624+paul27109@users.noreply.github.com>
2018-10-10 09:15:40 -0500

    Add files via upload

---
+++ Practica_10_10_2018/src/ec/edu/ister/vista/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.vista;
+
+/**
+ *
+ * @author PC3Lab2
+ */
+public class Aplicacion {
+   public static void main(){
+       System.out.println("hola mundo");
+   } 
+}
